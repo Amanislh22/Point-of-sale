9063d017339add1deb02b6e13a8304e1077790d0
ameni <salah.amani2001@gmail.com>
2024-09-07 23:36:32 +0100

    Product , category , customer , order and session management

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 from routers import auth, user,company,customer,category, product, session, order
 from database import engine, Base
+from firebase_admin import credentials, initialize_app
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("/home/ameni/Downloads/pos-project-8d951-firebase-adminsdk-u3f4p-ef9f3961c5.json")
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'gs://pos-project-8d951.appspot.com'
+})
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
